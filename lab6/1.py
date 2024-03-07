a8d224ef3fcd46d23f84adb3b8076a2f78971c55
Makhambet <idelbaevmakhambet860@gmail.com>
2024-03-08 00:40:21 +0500

    Create 1.py

---
+++ lab6/1.py
@@ -0,0 +1,7 @@
+from functools import reduce
+def multiply_list(numbers):
+    result = reduce(lambda x, y: x * y, numbers)
+    return result
+numbers_list = [2, 3, 4, 5]
+result = multiply_list(numbers_list)
+print(f"The product of the numbers in the list {numbers_list} is: {result}")
